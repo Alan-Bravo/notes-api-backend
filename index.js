446c25e403e725565e02432d21b72f70d983ff32
Alan Bravo <bravoalangonzalo@gmail.com>
2021-03-17 20:45:22 -0300

    Add Heroku needed config

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.post('/api/notes', (req, res) => {
   res.status(201).json(newNote);
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
